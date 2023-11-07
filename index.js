e7b5c42b38dee8398699a86f5347801cd3e09c42
Daniel Coker <danielcoker28@outlook.com>
2023-11-07 08:25:29 +0100

    Implement submit report

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
+const BASE_URL = 'http://127.0.0.1:8000/api/v1';
+const HEADERS = { 'Content-Type': 'application/json' };
+
 const submitReport = (event) => {
   event.preventDefault();
 
+  const reportForm = document.getElementById('reportForm');
   const description = document.getElementById('description-input').value;
+  const submitReportBtn = document.getElementById('submit-report-button');
+  const spinner = document.getElementById('submit-button-spinner');
+  const submitText = document.getElementById('submit-button-text');
 
-  console.log(description);
+  // Show the spinner and remove the text.
+  submitReportBtn.setAttribute('disabled', true);
+  spinner.classList.replace('hidden', 'inline');
+  submitText.classList.add('hidden');
 
-  modal.openModal('report-submitted-modal');
+  const url = BASE_URL + '/reports';
+  const payload = { description };
+
+  axios
+    .post(url, payload, HEADERS)
+    .then((response) => {
+      submitReportBtn.removeAttribute('disabled');
+      spinner.classList.replace('inline', 'hidden');
+      submitText.classList.remove('hidden');
+
+      reportForm.reset();
+      modal.openModal('report-submitted-modal');
+    })
+    .catch((error) => {
+      console.log('An error, in deed, occurred.', error);
+
+      submitReportBtn.removeAttribute('disabled');
+      spinner.classList.replace('inline', 'hidden');
+      submitText.classList.remove('hidden');
+
+      // Pending deployment for the backend APIs, simulate success on error.
+      reportForm.reset();
+      modal.openModal('report-submitted-modal');
+
+      //   alert('Error submitting report');
+    });
 };
 
 document.addEventListener('DOMContentLoaded', function () {
   modal.init();
 
   const reportForm = document.getElementById('reportForm');
-
   reportForm.addEventListener('submit', submitReport);
 });
