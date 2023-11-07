c5d723c0c1eab0dd33d9dab6a166f1ad5ac8de42
Daniel Coker <danielcoker28@outlook.com>
2023-11-07 10:46:05 +0100

    Update base urls

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000/api/v1';
+const BASE_URL = 'https://reportify-pqlb.onrender.com/api/v1';
 const HEADERS = { 'Content-Type': 'application/json' };
 
 const submitReport = (event) => {
