65a8de9c53fca16959a869229db475a68f16ada1
Daniel Coker <danielcoker28@outlook.com>
2023-11-08 06:06:36 +0100

    Take out backend warm up

--- scripts/index.js
+++ scripts/index.js
@@ -46,17 +46,6 @@ const submitReport = (event) => {
 document.addEventListener('DOMContentLoaded', function () {
   modal.init();
 
-  // Call the report lists endpoint to warm up the backend.
-  // The backend is currently deployed on Render. The issue with this
-  // is that it takes a while for the backend to spin up, so the first
-  // request to the backend always take a very long time.
-  // The solution is to make a request to the backend on page load so that
-  // the backend is ready (or close to ready) to handle requests when the user submits a report.
-  const url = BASE_URL + '/reports';
-  axios.get(url, HEADERS).catch((error) => {
-    console.log('An error, in deed, occurred.', error);
-  });
-
   const reportForm = document.getElementById('report-form');
   reportForm.addEventListener('submit', submitReport);
 });
